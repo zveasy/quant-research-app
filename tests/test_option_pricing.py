b827dfb175480389aa1155550f06b3c499037cac
Zakariya Veasy <zveasy327@gmail.com>
2025-07-14 16:39:41 -0700

    update

--- tests/test_option_pricing.py
+++ tests/test_option_pricing.py
@@ -1,5 +1,7 @@
 # In: tests/test_option_pricing.py
 
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytest
 from option_pricing.black_scholes import black_scholes_price
 
